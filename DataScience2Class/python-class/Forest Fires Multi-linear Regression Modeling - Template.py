58f1ec8698b8cce7f27fe107a745ae15f37c5df3
Ted Scott <fishdaddyo@gmail.com>
2023-05-26 08:41:59 -0700

    Adding DS2 course notebooks

---
+++ DataScience2Class/python-class/Forest Fires Multi-linear Regression Modeling - Template.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# import packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[2]:
+
+
+# read in forest fires dataset using pd.read_csv()
+
+
+# In[3]:
+
+
+# check out contents of the data frame using describe() and head()
+
+
+# In[4]:
+
+
+# create new column for natural log of (area + 1), called logArea
+
+
+# In[5]:
+
+
+# drop factor columns for month & day from data frame
+# build feature data frame and label data frame (one DF of feature columns, and one DF/list of label a.k.a. logArea)
+
+
+# In[6]:
+
+
+# confirm contents of feature and label data frames
+
+
+# In[7]:
+
+
+# (optional) make correlation plot using seaborn's heatmap
+
+
+# In[8]:
+
+
+# plot each feature column vs logArea using a scatterplot (e.g. seaborn's scatterplot)
+
+
+# In[9]:
+
+
+# create train/test split of data, 80/20 split
+
+
+# In[10]:
+
+
+# linear regression
+# import from sklearn, build model object, and use fit() to build model
+
+
+# In[11]:
+
+
+# output model coefficients
+
+
+# In[12]:
+
+
+# make predictions based on test data
+
+
+# In[13]:
+
+
+# Check model performance with RMSE
+
+
+# In[14]:
+
+
+# done!
+
